75d90768f0bfe8500f8d3a5265d8190ed75bef76
Louka Fauvel <louka.fauvel@gmail.com>
2021-06-04 10:15:19 +0200

    exo2

--- Tableau_PHP/index.php
+++ Tableau_PHP/index.php
@@ -1,4 +1,8 @@
 <?php
 $ingredients=['120g de beurre demi sel', '120g de chocolat', '100g de sucre', '80g de farine', '3x oeufs'];
+$ingredients=['beurre demi sel'=>120, 'chocolat'=>120, 'sucre'=>100, 'farine'=>80, 'oeufs'=>3];
+$unites=['grammes','grammes','grammes','grammes','x'];
 print_r($ingredients);
+echo "<br>";
+print_r($unites);
 ?>

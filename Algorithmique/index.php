538228b30e3eed61cd6f05744ee2c426950d4d57
Louka Fauvel <louka.fauvel@gmail.com>
2021-06-04 16:10:11 +0200

    feat : AlgoExo1

---
+++ Algorithmique/index.php
@@ -0,0 +1,12 @@
+<?php
+$N = [1, 2, 3, 4, 5, 6];
+$M = [1, 3, 6];
+
+$intersection = array_intersect($N, $M);
+
+print_r($N);
+echo "<br>";
+print_r($M);
+echo "<br><br>";
+print_r($intersection);
+?>
